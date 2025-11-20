41dceed6f6c9d41697f064eb40180cfece4cb7ff
angelfergar <anfernagar@gmail.com>
2025-11-20 23:44:21 +0100

    Headless added due to Gecko Update

--- base/web_factory.py
+++ base/web_factory.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 class WebDriverFactory():
-
     def get_webdriver(self, base_url):
-        driver = webdriver.Firefox()
+        options = Options()
+        options.headless = True
 
+        driver = webdriver.Firefox(options=options)
         driver.maximize_window()
         driver.get(base_url)
-
         return driver
