c4301d1e9e413ab145cf12403f20a127a2787cec
angelfergar <anfernagar@gmail.com>
2025-11-20 23:36:35 +0100

    Removed headless from web_factory

--- base/web_factory.py
+++ base/web_factory.py
@@ -1,14 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-
 
 class WebDriverFactory():
 
     def get_webdriver(self, base_url):
-        options = Options()
-        options.headless = True
+        driver = webdriver.Firefox()
 
-        driver = webdriver.Firefox(options=options)
-
+        driver.maximize_window()
         driver.get(base_url)
+
         return driver
