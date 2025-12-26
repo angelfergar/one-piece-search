97611992c2d269f68448f8dcad1ce4458e14208a
angelfergar <anfernagar@gmail.com>
2025-12-26 19:43:34 +0100

    Ignore chapter if not in spanish

--- webs/anime_allstar.py
+++ webs/anime_allstar.py
@@ -12,6 +12,7 @@ class AllStar(BasePage):
 
     # Locators
     _link_chapters = "//div[@class='ast-post-format- blog-layout-4 ast-article-inner']"
+    _extra_message = "//div[@class='ast-excerpt-container ast-blog-single-element']"
     _title_chapters = "//a[@rel='bookmark']"
     _chapter_images = "//div[@class='separator']"
 
@@ -21,13 +22,19 @@ class AllStar(BasePage):
         original_window = self.driver.current_window_handle
         self.wait_for_element(self._link_chapters, condition="visible")
         list_of_titles = self.get_elementList(self._title_chapters)
+        list_of_messages = self.get_elementList(self._extra_message)
         for  i in range(chapters_checked):
             container = list_of_titles[i]
-            title_text = self.get_text(element=container)
-            chapter_listed = self.utils.verify_text_contains(chapter, title_text)
-            if chapter_listed:
-                self.element_click(element=container)
-                break
+            info_message = list_of_messages[i]
+            print(self.get_text(element=info_message))
+            if info_message:
+                continue
+            else:
+                title_text = self.get_text(element=container)
+                chapter_listed = self.utils.verify_text_contains(chapter, title_text)
+                if chapter_listed:
+                    self.element_click(element=container)
+                    break
         # There's a pop up, so we have to return to the One Piece tab
         all_windows = self.driver.window_handles
         for handle in all_windows:
