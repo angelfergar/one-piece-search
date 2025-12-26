38516e8664bd76843b00395f772974f318a23c7e
angelfergar <anfernagar@gmail.com>
2025-12-26 20:15:46 +0100

    Back to manual WebDriver Manager

--- base/web_factory.py
+++ base/web_factory.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
 
 class WebDriverFactory():
     def get_webdriver(self, base_url):
@@ -9,13 +7,7 @@ class WebDriverFactory():
         options.headless = True
         options.add_argument("--headless")
 
-        service = Service(GeckoDriverManager().install())
-
-        driver = webdriver.Firefox(
-            service=service,
-            options=options
-        )
-
+        driver = webdriver.Firefox(options=options)
         driver.set_window_size(1920, 1080)
         driver.get(base_url)
         return driver
