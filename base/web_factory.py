89864dcd63f313511200023aa86f2acf2d29465e
angelfergar <anfernagar@gmail.com>
2026-01-03 19:38:05 +0100

    Headless fix

--- base/web_factory.py
+++ base/web_factory.py
@@ -4,7 +4,7 @@ from selenium.webdriver.firefox.options import Options
 class WebDriverFactory():
     def get_webdriver(self, base_url):
         options = Options()
-        options.headless = True
+        options.add_argument("-headless")
 
         driver = webdriver.Firefox(options=options)
         driver.set_window_size(426, 240)
